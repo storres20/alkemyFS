d813fb486ecc0cc3601084758228fe28b4afc42e
storres20 <sanchotorres777@gmail.com>
2022-06-26 15:19:53 -0500

    feat(web): add auth0 Login page

--- Front-End/web/src/pages/Login/Login.jsx
+++ Front-End/web/src/pages/Login/Login.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 import logo from './logo.svg'
 import '../../App.css'
 
+import { useAuth0 } from '@auth0/auth0-react'
+
+import { LoginButton } from '../../components/LoginButton/LoginButton'
+import { LogoutButton } from '../../components/LogoutButton/LogoutButton'
+import { Profile } from '../../components/Profile/Profile'
+
 function Login() {
+  const {isAuthenticated} = useAuth0()
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,15 +18,15 @@ function Login() {
         <p>
           Welcome to your Personal Finance App.
         </p>
-        {/* <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Login
-        </a> */}
-        <button className='btn btn-info'>Login</button>
+        
+        {isAuthenticated ? 
+          <>
+          <Profile/>
+          <LogoutButton/>
+          </>
+          :
+          <LoginButton/>}
+        
       </header>
     </div>
   )
