bcb740225ffdf56c8dff3409c9d8db17623fd46d
storres20 <sanchotorres777@gmail.com>
2022-06-26 16:51:00 -0500

    feat(web): add routes to app.js

--- Front-End/web/src/App.js
+++ Front-End/web/src/App.js
@@ -1,11 +1,21 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Login from "./pages/Login/Login";
+import Home from "./pages/Home/Home"
+import NotFoundPage from './pages/NotFoundPage/NotFoundPage';
+
+import {Routes, Route} from 'react-router-dom'
 
 function App() {
   return (
     <div>
-      <Login/>
+      <main>
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/*" element={<NotFoundPage />} />
+          </Routes>
+        </main>
     </div>
   );
 }
