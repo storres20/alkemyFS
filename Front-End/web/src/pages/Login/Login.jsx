92f2380f6465c8af0c43fc081666070a1b475c3a
storres20 <sanchotorres777@gmail.com>
2022-06-26 11:47:00 -0500

    feat(web): add login's logo

--- Front-End/web/src/pages/Login/Login.jsx
+++ Front-End/web/src/pages/Login/Login.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import logo from './logo.svg'
+import '../../App.css'
 
 function Login() {
   return (
-    <div>Login</div>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Welcome to your Personal Finance App.
+        </p>
+        {/* <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Login
+        </a> */}
+        <button className='btn btn-info'>Login</button>
+      </header>
+    </div>
   )
 }
 
