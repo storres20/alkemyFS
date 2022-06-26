12572eab8b7515e656d668f5bec13f02dd64208f
storres20 <sanchotorres777@gmail.com>
2022-06-26 16:47:19 -0500

    fix(web): fix login page

--- Front-End/web/src/pages/Login/Login.jsx
+++ Front-End/web/src/pages/Login/Login.jsx
@@ -1,34 +1,11 @@
 import React from 'react'
-import logo from './logo.svg'
-import '../../App.css'
+import LoginForm from '../../components/LoginForm/LoginForm'
 
-import { useAuth0 } from '@auth0/auth0-react'
-
-import { LoginButton } from '../../components/LoginButton/LoginButton'
-import { LogoutButton } from '../../components/LogoutButton/LogoutButton'
-import { Profile } from '../../components/Profile/Profile'
 
 function Login() {
-  const {isAuthenticated} = useAuth0()
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to your Personal Finance App.
-        </p>
-        
-        {isAuthenticated ? 
-          <>
-          <Profile/>
-          <LogoutButton/>
-          </>
-          :
-          <LoginButton/>}
-        
-      </header>
-    </div>
+    <LoginForm/>
   )
 }
 
