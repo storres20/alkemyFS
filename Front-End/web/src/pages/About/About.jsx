afe9918f352303bd867ab7339ce4792b6d0fa5fb
storres20 <sanchotorres777@gmail.com>
2022-07-17 21:04:32 -0500

    feat(web): add about page

---
+++ Front-End/web/src/pages/About/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function About() {
+  return (
+    <div>
+      <Link to="/home">Home</Link>
+      <h1>About</h1>
+    </div>
+  )
+}
+
+export default About
