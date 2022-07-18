6181e017cb4518a0c317edeec1d1388eadbf4262
storres20 <sanchotorres777@gmail.com>
2022-07-17 21:03:35 -0500

    feat(web): add localstorage

--- Front-End/web/src/App.js
+++ Front-End/web/src/App.js
@@ -1,19 +1,44 @@
+import React, {useState, useEffect} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Login from "./pages/Login/Login";
 import Home from "./pages/Home/Home"
-import NotFoundPage from './pages/NotFoundPage/NotFoundPage';
+import About from './pages/About/About';
 
-import {Routes, Route} from 'react-router-dom'
+import {Routes, Route, Navigate} from 'react-router-dom'
 
 function App() {
+  const storedUser = JSON.parse(localStorage.getItem("user")) || false;
+
+  const [user, setUser] = useState(storedUser)
+  
+  useEffect(() => {
+    const u = JSON.parse(localStorage.getItem("user"))
+    //console.log(u)
+    u ? setUser(true) : setUser(false)
+  }, [])
+  
+  useEffect(() => {
+    localStorage.setItem("user", JSON.stringify(user))
+  }, [user])
+
+
   return (
     <div>
       <main>
           <Routes>
-            <Route path="/" element={<Login />} />
-            <Route path="/home" element={<Home />} />
-            <Route path="/*" element={<NotFoundPage />} />
+            {!user && (
+              <Route path="/" element={<Login authenticate={() => setUser(true)} />} />
+            )}
+            
+            {user && (
+              <>
+                <Route path="/home" element={<Home logout={() => setUser(false)} />} />
+                <Route path="/about" element={<About />} />
+              </>
+            )}
+            
+            <Route path="/*" element={<Navigate to={user ? "/home" : "/"} />} />
           </Routes>
         </main>
     </div>
