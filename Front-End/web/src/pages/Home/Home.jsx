7068034c4f0f1961fbe6ff8b6ed81973f7c0ea53
storres20 <sanchotorres777@gmail.com>
2022-07-17 23:31:34 -0500

    fix(web): change home pages

--- Front-End/web/src/pages/Home/Home.jsx
+++ Front-End/web/src/pages/Home/Home.jsx
@@ -1,12 +1,41 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import NavBar from '../../components/NavBar/NavBar'
+
+import './Home.scss'
+
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+
 
 function Home({logout}) {
   return (
     <div>
-      <Link to="/about">About</Link>
-      <h1>Home</h1>
-      <button onClick={logout}>Logout</button>
+      <NavBar logout = {logout} />
+      
+      <Card>
+        <Card.Body>
+          <Card.Title>Home</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Button variant="primary"><i className="bi bi-plus-circle"></i> New</Button>
+          
+          <InputGroup className="mt-3 mb-3">
+            <InputGroup.Text id="basic-addon1">Total balance:</InputGroup.Text>
+            <Form.Label className='label'><span>$ 100</span></Form.Label>
+          </InputGroup>
+          
+          <Card.Title>Financial List:</Card.Title>
+          
+          
+          
+        </Card.Body>
+      </Card>
+      
     </div>
   )
 }
