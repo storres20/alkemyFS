699ca7a1154e639fe2b84cb4e4fea02d463307c7
storres20 <sanchotorres777@gmail.com>
2022-07-25 15:58:08 -0500

    fix(web): change card.text

--- Front-End/web/src/pages/Edit/Edit.jsx
+++ Front-End/web/src/pages/Edit/Edit.jsx
@@ -105,8 +105,7 @@ function Edit({logout}) {
         <Card.Body className="flex1">
           <Card.Title><h1>Edit</h1></Card.Title>
           <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
+            Feel free to edit your financial incomes or outcomes.
           </Card.Text>
           
           {currentProduct ? (
